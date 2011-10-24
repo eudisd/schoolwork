d212082d17e6786f31f04840610c582abae5929b
Eudis Duran <eudisd@live.com>
2011-10-24 16:46:11 -0400

    Started working on lab6 and also, added a few files to my repos

--- comp-org/lab5/ps2_receiver/ps2_rec.vhd
+++ comp-org/lab5/ps2_receiver/ps2_rec.vhd
@@ -1,9 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 entity ps2_rec is
-	port(
+	port
+	(
 	clk ,reset: in std_logic;
 	ps2data : in std_logic;
 	ps2clock : in std_logic;
@@ -15,16 +15,14 @@ end ps2_rec;
 
 architecture arch of ps2_rec is
 	type statetype is (idle, receive, done);
-	
 	signal current_state, next_state : statetype;
 	signal current_data, next_data: std_logic_vector(10 downto 0);
-	signal u_current_data : unsigned(10 downto 0);
-	signal current_n, next_n: unsigned(3 downto 0);
+	signal current_n, next_n : unsigned(3 downto 0) := "1001";
 	signal fall_edge: std_logic;
 	signal current_filter, next_filter : std_logic_vector(7 downto 0);
 	signal current_filter_clock, next_filter_clock : std_logic;
-	
 begin
+
 	process(clk, reset)
 	begin
 		if reset = '1' then
@@ -35,15 +33,15 @@ begin
 			current_filter_clock <= next_filter_clock;
 		end if;
 	end process;
-	
-	next_filter <= ps2clock & current_filter(7 downto 1);
-	next_filter_clock <= '1' when current_filter = "11111111" else
-	'0' when current_filter = "00000000" else
-	current_filter_clock;
-	fall_edge <= current_filter_clock and (not next_filter_clock);
-	
+		
+		next_filter <= ps2clock & current_filter(7 downto 1);
+		next_filter_clock <= '1' when current_filter = "11111111" else
+		'0' when current_filter = "00000000" else
+		current_filter_clock;
+		fall_edge <= current_filter_clock and (not next_filter_clock);
+		
 	process (clk, reset)
-		begin
+	begin
 			if(reset = '1') then
 				current_state <= idle;
 				current_n <= (others => '0');
@@ -53,46 +51,39 @@ begin
 				current_n <= next_n;
 				current_data <= next_data;
 			end if;
-		end process;
-		
-		process(current_state, current_data, current_n, ps2data, fall_edge, rec_en)
-		begin
-			rec_done <= '0';
-			next_state <= current_state;
-			next_n <= current_n;
-			next_data <= current_data;
-		case current_state is
+
+	end process;
+	process(current_state, current_data, current_n, ps2data, fall_edge)
+	begin
+		rec_done <= '0';
+		next_state <= current_state;
+		next_n <= current_n;
+		next_data <= current_data;
 			--write the code for idle and receive states
 			--in idle state, when the receiver is enabled and on falling edge of the ps2
 			--clock (fall_edge = '1'), shift the (current)data to the right and set the most
 			--significant bit of the (next)data to ps2 data. also set n to be equal to nine.
 			--remember to decrement n in the receive state.
-		when idle =>
-			if(fall_edge = '1' and rec_en = '1') then
-					next_data(9 downto 0) <= current_data(10 downto 1);
-					next_data(10) <= ps2data;
-					next_n <= to_unsigned(9, 4);
-					next_state <= receive;
-			end if;
-		when receive =>
-			if(fall_edge = '1' and rec_en = '1') then
-				if( current_n = to_unsigned(0, 4) ) then
-					-- One last time
-					next_data(9 downto 0) <= current_data(10 downto 1);
-					next_data(10) <= ps2data;
-					next_state <= done;
-				else
-					next_data(9 downto 0) <= current_data(10 downto 1);
-					next_data(10) <= ps2data;
-					next_n <= next_n - to_unsigned(1, 4);
-				end if;
-			end if;
-				
-		when done =>
-			next_state <= idle;
-			rec_done <= '1';
-		end case;
+		if( fall_edge = '1' and rec_en = '1' ) then
+			case current_state is
+				when idle =>
+						next_data <= ps2data & current_data(10 downto 1);
+						next_state <= receive;
+						next_n <= "1001";
+				when receive =>
+						if ( current_n = "0000" ) then
+							next_data <= ps2data & current_data(10 downto 1);
+							next_state <= done;
+						else
+							next_data <= ps2data & current_data(10 downto 1);
+							next_n <= next_n - 1;
+						end if;
+				when done =>
+					next_state <= idle;
+					rec_done <= '1';
+			end case;
+		end if;
 	end process;
-	
 	dout <= current_data(8 downto 1);
+
 end arch;
---
+++ comp-org/lab6/ps2_tran.vhd
@@ -0,0 +1,133 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ps2_tran is
+	port (clk ,reset: in std_logic;
+	ps2_data : inout std_logic;
+	ps2_clk : inout std_logic;
+	en : std_logic;
+	tran_done: out std_logic;
+	tran_idle: out std_logic;
+	din: in std_logic_vector( 7 downto 0));
+end ps2_tran;
+
+architecture arch of ps2_tran is
+	type statetype is (idle, request, start, transmit, done);
+	signal current_state, next_state : statetype;
+	signal current_counter, next_counter : unsigned(12 downto 0);
+	signal current_data, next_data: std_logic_vector(8 downto 0);
+	signal current_n, next_n : unsigned(3 downto 0);
+	signal parity : std_logic;
+	signal ps2_clk_out, ps2_data_out : std_logic;
+	signal tri_clk, tri_data : std_logic;
+	signal fall_edge: std_logic;
+	signal current_filter, next_filter : std_logic_vector(7 downto 0);
+	signal current_filter_clock, next_filter_clock : std_logic;
+begin
+	process(clk, reset)
+	begin
+		if reset = '1' then
+			current_filter <= (others => '0');
+			current_filter_clock <= '0';
+		elsif (rising_edge(clk)) then
+			current_filter <= next_filter;
+			current_filter_clock <= next_filter_clock;
+		end if;
+	end process;
+	
+	next_filter <= ps2_clk & current_filter(7 downto 1);
+	next_filter_clock <= '1' when current_filter = "11111111" else
+	'0' when current_filter = "00000000" else
+	current_filter_clock;
+	fall_edge <= current_filter_clock and (not next_filter_clock);
+	
+	process (clk, reset)
+	begin
+		if(reset = '1') then
+			current_state <= idle;
+			current_n <= (others => '0');
+			current_data <= (others => '0');
+			current_counter <= (others => '0');
+		elsif(rising_edge(clk)) then
+			current_state <= next_state;
+			current_n <= next_n;
+			current_data <= next_data;
+			current_counter <= next_counter;
+		end if;
+	end process;
+	
+	parity <= not (din(7) xor din (6) xor din (5) 
+	          xor din (4) xor din (3) xor din(2) 
+			  xor din (1)xor din (0));
+	
+	process(current_state, 
+			current_data, 
+			current_n, 
+			current_counter, 
+			en, 
+			din,
+			parity, 
+			fall_edge)
+	begin
+		tran_done <= '0';
+		ps2_clk_out <= '1';
+		ps2_data_out <= '1';
+		tri_clk <= '0';
+		tri_data <= '0';
+		tran_idle <= '0';
+		next_state <= current_state;
+		next_n <= current_n;
+		next_data <= current_data;
+		next_counter <= current_counter;
+		
+		case current_state is
+			when idle =>
+				tran_idle <='1';
+				if(en = '1') then
+					next_data <= parity & din;
+					next_counter <= (others => '1');
+					next_state <= request;
+				end if;
+			when request =>
+				ps2_clk_out <= --enter you code here;
+				tri_clk <= '1';
+				next_counter <= current_counter - 1;
+				if (current_counter = 0) then
+					next_state <= start;
+				end if;
+			when start =>
+				ps2_data_out <= --enter you code here;
+				tri_data <= '1';
+				
+				if (fall_edge = '1') then
+					next_n <= "1000";
+					next_state <= transmit;
+				end if;
+				
+			when transmit =>
+			
+				ps2_data_out <= --enter you code here;
+				tri_data <= '1';
+				
+				if (fall_edge = '1') then
+					next_data <= '0' & --enter you code here;
+					if current_n = 0 then
+						next_state <= --enter you code here;
+					else
+						next_n <= --enter you code here;
+					end if;
+				end if;
+				
+			when done =>
+				if (fall_edge = '1') then
+					next_state <= idle;
+					tran_done <= '1';
+				end if;
+		end case;
+	end process;
+	
+	ps2_clk <= ps2_clk_out when tri_clk = '1' else 'z';
+	ps2_data <= ps2_data_out when tri_data = '1' else 'z';
+	
+end arch;
