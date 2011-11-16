3502b46c284da49adf8e87272a2214444bceff86
Eudis Duran <eudisd@gmail.com>
2011-11-15 22:19:57 -0500

    progress...

--- comp-org/projects/optimization/main.c
+++ comp-org/projects/optimization/main.c
@@ -6,68 +6,56 @@
 #include "lib/iter.h"
 #include "lib/ptr.h"
 
-#define RANGE 8000000
+#define RANGE 80000
 
-__uint64_t rdtsc();
 double microsecs(void);
 
 int main(void){
-    __uint64_t t1, t2, diff = 0;
+    double t1, t2, diff = 0;
 
     int i;
     for(i = 0; i < RANGE; i++){
-        t1 = rdtsc();
+        t1 = microsecs();
         iter();
-        t2 = rdtsc();
+        t2 = microsecs();
         diff = diff + (t2 - t1);
     }
-    diff /= (__uint64_t)RANGE;
+    diff /= (double)RANGE;
     
-    printf("Unoptimized iter() function call: %u\n", diff);
+    printf("Unoptimized iter() function call: %0.8f\n", diff);
     for(i = 0, diff = 0; i < RANGE; i++){
-        t1 = rdtsc();
+        t1 = microsecs();
         ptr();
-        t2 = rdtsc();
+        t2 = microsecs();
         diff = diff + (t2 - t1);
     }
 
-    diff /= (__uint64_t)RANGE;
-    printf("Unoptimized ptr() function call: %u\n", diff);
+    diff /= (double)RANGE;
+    printf("Unoptimized ptr() function call: %0.8f\n", diff);
 
     // Optimized versions of these functinos 
     
     for(i = 0, diff = 0; i < RANGE; i++){
-        t1 = rdtsc();
+        t1 = microsecs();
         opt_iter();
-        t2 = rdtsc();
+        t2 = microsecs();
         diff = diff + (t2 - t1);
     }
-    diff /= (__uint64_t)RANGE;
-    printf("Unoptimized opt_iter() function call: %u\n", diff);
+    diff /= (double)RANGE;
+    printf("Unoptimized opt_iter() function call: %0.8f\n", diff);
     
     for(i = 0, diff = 0; i < RANGE; i++){
-        t1 = rdtsc();
+        t1 = microsecs();
         opt_ptr();
-        t2 = rdtsc();
+        t2 = microsecs();
         diff = diff + (t2 - t1);
     }
-    diff /= (__uint64_t)RANGE;
-    printf("Unoptimized opt_ptr() function call: %u\n", diff);
+    diff /= (double)RANGE;
+    printf("Unoptimized opt_ptr() function call: %0.8f\n", diff);
     
     return 0;
 }
-inline __uint64_t rdtsc(){
-    __uint32_t lo, hi;
-    asm volatile (
-            "xorl %%eax, %%eax\n"
-            "cpuid\n"
-            "rdtsc\n"
-            : "=a" (lo), "=d" (hi)
-            :
-            : "%ebx", "%ecx");
-    return (long)hi << 32 | lo;
 
-}
 double microsecs(){
     static struct timeval _t;
     static struct timezone tz;
