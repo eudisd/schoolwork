46a4ad147b29b874980bc8e710b2e5deb27fac0d
Eudis Duran <eudisd@gmail.com>
2011-11-26 16:48:34 -0500

    Finished optimization problem

--- comp-org/projects/optimization/lib/ptr.s
+++ comp-org/projects/optimization/lib/ptr.s
@@ -1,9 +1,15 @@
+	.file	"ptr.c"
 	.text
-	.globl	opt_ptr
-opt_ptr:
+	.globl	ptr
+	.type	ptr, @function
+ptr:
 .LFB0:
+	.cfi_startproc
 	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
 	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
 	subl	$4000016, %esp
 	leal	-4000004(%ebp), %eax
 	movl	%eax, -4(%ebp)
@@ -18,4 +24,11 @@ opt_ptr:
 	cmpl	%eax, -4(%ebp)
 	jb	.L3
 	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
 	ret
+	.cfi_endproc
+.LFE0:
+	.size	ptr, .-ptr
+	.ident	"GCC: (Ubuntu/Linaro 4.6.1-9ubuntu3) 4.6.1"
+	.section	.note.GNU-stack,"",@progbits
