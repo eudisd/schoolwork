ff24c0182f65d763d7ddd361541a8b3f304d81e0
Eudis Duran <eudisd@live.com>
2011-12-04 15:17:38 -0500

    Started work on control_unit

--- comp-org/lab8/control_unit.vhd
+++ comp-org/lab8/control_unit.vhd
@@ -20,7 +20,35 @@ entity control_unit is
 end control_unit;
 
 architecture control_arch of control_unit is
-
+	type state_type is (cpu_wait, cpu_off, fetch, decode, mov, 
+	                   movi, operation, storeA, AddG, SubG, finished);
+					   
+	signal current_state, next_state : state_type;
 begin
 
+	-- States Process
+	process(current_state)
+	begin
+		case current_state is
+			when cpu_wait =>
+			when cpu_off =>
+			when fetch =>
+			when decode =>
+			when mov =>
+			when movi =>
+			when operation =>
+			when storeA =>
+			when AddG =>
+			when SubG =>
+			when finished =>
+		end case;
+	end process;
+	
+	-- CPU read/write process
+	process (Clock, Reset)
+	begin
+		current_state <= next_state;
+		
+		
+	end process;
 end control_arch;
