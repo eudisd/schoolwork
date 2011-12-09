a20a9f8cb521769781484cc680932eab7c66e564
Eudis Duran <eudisd@gmail.com>
2011-12-08 22:38:23 -0500

    Finished template for control unit test script

---
+++ comp-org/lab8/test_control_unit.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test_control_unit is
+    port();
+end test_control_unit;
+
+architecture test_arch of test_control_unit is
+begin
+
+end test_arch;
+
