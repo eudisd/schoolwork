a874dab4d14e995d6037c2d6c4e01b58a775af29
Eudis Duran <eudisd@gmail.com>
2011-12-11 18:43:37 -0500

    ..

--- comp-org/projects/simd/simd.c
+++ comp-org/projects/simd/simd.c
@@ -11,12 +11,20 @@ int main(void)
 {
     srand(time(NULL)); // seed the rand number generator
 	
-	mat4x4 *bigMatA = create4x4Array(ARRAY_SIZE); 
-	mat4x4 *bigMatB = create4x4Array(ARRAY_SIZE); 	
-	mat4x4 *bigMatRes = create4x4ClearArray(ARRAY_SIZE);
+	//mat4x4 *bigMatA = create4x4Array(ARRAY_SIZE); 
+	//mat4x4 *bigMatB = create4x4Array(ARRAY_SIZE); 	
+	//mat4x4 *bigMatRes = create4x4ClearArray(ARRAY_SIZE);
 	
-	multBigMatSIMD(bigMatRes, ARRAY_SIZE, bigMatA, bigMatB);
+	//multBigMatSIMD(bigMatRes, ARRAY_SIZE, bigMatA, bigMatB);
+	mat4x4 a = create4x4();
+	mat4x4 b = create4x4();
+	mat4x4 res = create4x4Clear();
 	
+	print4x4(a);
+	print4x4(b);
+	mult4x4SIMD(res, a, b);
+	print4x4(res);
+
     return 0;
 }
 
