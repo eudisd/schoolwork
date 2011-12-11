18bc17e6e7aadc8ee24fdf474ddfc5a8d582a357
Eudis Duran <eudisd@gmail.com>
2011-12-11 15:57:36 -0500

    Fixed seg fault.  I guess my c is good :)

--- comp-org/projects/simd/iter.c
+++ comp-org/projects/simd/iter.c
@@ -5,7 +5,7 @@
 
 #include "mat.h" // Includes all matrix operations functions
 
-const int ARRAY_SIZE = 16000; // 4*4*16000 elements!
+const int ARRAY_SIZE = 6; // 4*4*16000 elements!
 
 int main(void)
 {
@@ -15,7 +15,11 @@ int main(void)
 	mat4x4 *bigMatA = create4x4Array(ARRAY_SIZE); 
 	mat4x4 *bigMatB = create4x4Array(ARRAY_SIZE); 	
 	mat4x4 *bigMatRes = create4x4ClearArray(ARRAY_SIZE);
-	multBigMat(bigMatRes, ARRAY_SIZE, bigMatA, bigMatB);
+	print4x4(bigMatA[0]);
+	int i;
+	//for(i = 0; i < 6; i++)
+	//	print4x4(bigMatA[i]);
+	//multBigMat(bigMatRes, ARRAY_SIZE, bigMatA, bigMatB);
 	
     return 0;
 }
